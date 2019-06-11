ed86a33e9a3c3134e48d85eb6378efe79113ab9a
jovanw2941 <51722074+jovanw2941@users.noreply.github.com>
2019-06-11 16:38:59 -0400

    Add files via upload

---
+++ mad libs game.py
@@ -0,0 +1,9 @@
+color= input("Enter a color: ")
+color2=input("Enter another color: ")
+plural_noun= input("Enter a plural noun: ")
+celebrity= input("Enter a name for a celebrity: ")
+plural_noun2= input("Enter the name of another plural noun: ")
+
+print( plural_noun2 + " are " + color )
+print(plural_noun + " are " + color2)
+print("I love " + celebrity) 
